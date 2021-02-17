0b892dbe8b1b26403b6d7f9866719bbfabf39215
Minemug <34980966+Minemug@users.noreply.github.com>
2021-02-17 21:59:00 +0100

    AddedDeletingProductsFromBasketCodeLogic

--- WpfApp1/LoggedIn.xaml.cs
+++ WpfApp1/LoggedIn.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 
 namespace WpfApp1
@@ -22,24 +23,36 @@ namespace WpfApp1
     public partial class LoggedIn : Window
     {
         ShopEntities db = new ShopEntities();
-        
+        CollectionViewSource basketViewSource;
         public LoggedIn()
         {
             InitializeComponent();
+            basketViewSource = ((CollectionViewSource)(FindResource("basketViewSource")));
+            DataContext = this;
         }
         
         private void AddToBasketButton_Click(object sender, RoutedEventArgs e)
         {
-            var quantity = Convert.ToInt32(QuantityTextBox.Text);
-            var index = ComboBox.SelectedIndex + 1;//W sqlu index zaczyna sie od 1
-            int ClientID = Convert.ToInt32(Passdata.Text);
-            var basket = db.Set<Basket>();
-            if(quantity>0)
+            try
             {
-                basket.Add(new Basket {ProductID = index, Quantity = quantity, ClientID = ClientID});
-                db.SaveChanges();
-                MessageBox.Show("Dodano produkt");
+                var quantity = Convert.ToInt32(QuantityTextBox.Text);
+                var index = ComboBox.SelectedIndex + 1;//W sqlu index zaczyna sie od 1
+                int ClientID = Convert.ToInt32(Passdata.Text);
+                var basket = db.Set<Basket>();
+                if(quantity>0)
+                {
+                    basket.Add(new Basket {ProductID = index, Quantity = quantity, ClientID = ClientID});
+                    db.SaveChanges();
+                    MessageBox.Show("Dodano produkt");
+                }
+
             }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Podano nieprawidłowy format wartości ilosci lub nie podano wgle");
+            }
+            ComboBoxDelete.Items.Clear();
+            FillDeleteCombobox();
         }
 
         private void ComboBox_Initialized(object sender, EventArgs e)
@@ -52,18 +65,58 @@ namespace WpfApp1
         }
 
 
-        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
-        {
-            int ClientID = Convert.ToInt32(Passdata.Text);
-            IEnumerable<Basket> basket = db.Basket.Local;
-            DataGrid.ItemsSource = basket;
-        }
+     
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MainWindow loginWindow = new MainWindow();
             loginWindow.Show();
             this.Close();
+        }
+
+        private void LoggedIn1_Loaded(object sender, RoutedEventArgs e)
+        {
+            db.Basket.Load();
+            basketViewSource.Source = db.Basket.Local;
+        }
+        private void DeleteFromBasketButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var index = ComboBoxDelete.SelectedIndex;
+            var ord = (from o in db.Basket.Local
+                    where o.BasketID == indexlist[index]
+                    select o).FirstOrDefault();
+            db.Basket.Remove(ord);
+            db.SaveChanges();
+            ComboBoxDelete.Items.Clear();
+            FillDeleteCombobox();
+        }
+
+        int[] indexlist = new int[50];
+        int i = 0;
+        private void ComboBoxDelete_Loaded(object sender, RoutedEventArgs e)
+        {
+            FillDeleteCombobox();
+        }
+
+        private void FillDeleteCombobox()
+        {
+            
+            foreach (var item in db.Basket)
+            {
+                if (item.ClientID == Convert.ToInt32(Passdata.Text))
+                {
+                    foreach (var prod in db.Products)
+                    {
+                        if (item.ProductID == prod.ProductID)
+                        {
+                            ComboBoxDelete.Items.Add($"{prod.ProductName} ilosc {item.Quantity}");
+                            indexlist[i] = item.BasketID;
+                            i++;
+                        }
+                    }
+                }
+            }
         }
     }
 }
